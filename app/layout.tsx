d3a02ebabefec2efe0e1d6be2e20ce2332d88d4f
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2025-04-18 14:38:27 +0530

    upgrade to v10

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import { Navbar } from "@/components/Navbar";
-import { ToastContainer } from "react-toastify";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -33,7 +32,6 @@ export default async function RootLayout({
         <main className="flex flex-col max-w-screen-lg mx-auto pb-20">
           <Providers>
             <Navbar />
-            <ToastContainer />
             {children}
           </Providers>
         </main>
--- app/providers.tsx
+++ app/providers.tsx
@@ -2,51 +2,50 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { type ReactNode, useState } from "react";
-import { createConfig, http, WagmiProvider } from "wagmi";
-import { mainnet, sepolia, lineaSepolia } from "wagmi/chains";
-import { MetaMaskSDK } from "@metamask/sdk";
-import { useEffect } from "react";
-import Web3AuthConnectorInstance from "./web3authConnector";
-import { IEthereum } from "./types/ethereum";
+
+import { Web3AuthProvider } from "@web3auth/modal/react";
+import { WagmiProvider } from "@web3auth/modal/react/wagmi";
+
+// import { MetaMaskSDK } from "@metamask/sdk";
 
 type Props = {
   children: ReactNode;
 };
 
-const config = createConfig({
-  chains: [mainnet, sepolia, lineaSepolia],
-  transports: {
-    [mainnet.id]: http(),
-    [sepolia.id]: http(),
-    [lineaSepolia.id]: http(),
-  },
-  connectors: [Web3AuthConnectorInstance([mainnet, sepolia, lineaSepolia])],
-});
-
 export function Providers({ children }: Props) {
   const [queryClient] = useState(() => new QueryClient());
 
-  useEffect(() => {
-    if (typeof window === "undefined") return;
+  // useEffect(() => {
+  //   if (typeof window === "undefined") return;
 
-    const MMSDK = new MetaMaskSDK({
-      dappMetadata: {
-        name: "MetaMask Web3Auth Integration",
-        url: window.location.href,
-      },
-    });
+  //   const MMSDK = new MetaMaskSDK({
+  //     dappMetadata: {
+  //       name: "MetaMask Web3Auth Integration",
+  //       url: window.location.href,
+  //     },
+  //   });
 
-    const ethereum = MMSDK.getProvider();
-    if (ethereum) {
-      window.ethereum = ethereum as unknown as IEthereum;
-    }
-  }, []);
+  //   const ethereum = MMSDK.getProvider();
+  //   if (ethereum) {
+  //     window.ethereum = ethereum as unknown as IEthereum;
+  //   }
+  // }, []);
 
   return (
-    <WagmiProvider config={config as any}>
+    <Web3AuthProvider
+      config={{
+        web3AuthOptions: {
+          clientId: process.env.NEXT_PUBLIC_WEB3AUTH_CLIENT_ID!, // Replace with your Client ID
+          web3AuthNetwork: "sapphire_devnet",
+          authBuildEnv: "testing", // Optional: Only for alpha/testing
+        },
+      }}
+    >
       <QueryClientProvider client={queryClient}>
-        <div className="container">{children}</div>
+        <WagmiProvider>
+          <div className="container">{children}</div>
+        </WagmiProvider>
       </QueryClientProvider>
-    </WagmiProvider>
+    </Web3AuthProvider>
   );
 }
